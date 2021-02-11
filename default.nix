433b23b07e3d8ed6e39a1ba065bc0643fa3c2a94
Emanuele Parrinello <parry84@gmail.com>
2021-02-11 18:09:00 +0100

    Post-meetup improvements to Chapter-24

--- default.nix
+++ default.nix
@@ -3,5 +3,15 @@ let
 in import "${pkgs.ihaskell}/release.nix" {
   compiler = "ghc865";
   nixpkgs = import pkgs.nixpkgs {};
-  packages = self: with self; [ hspec QuickCheck checkers hedis scotty ];
+  packages = self: with self; [ 
+    hspec
+    QuickCheck
+    checkers
+    hedis
+    scotty
+    trifecta
+    raw-strings-qq
+    generic-random
+    graphviz
+  ];
 }
